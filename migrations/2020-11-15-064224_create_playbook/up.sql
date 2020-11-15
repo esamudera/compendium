b5dd9f086e408a6aa3f0aea461d1e68dab2cc06a
Ericko Samudera <ericko.samudera@gmail.com>
2020-11-15 13:51:20 +0700

    [db] create playbook table

---
+++ migrations/2020-11-15-064224_create_playbook/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE playbook (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  create_time INTEGER,
+  update_time INTEGER
+)
---
+++ src/schema.rs
@@ -0,0 +1,8 @@
+table! {
+    playbook (id) {
+        id -> Nullable<Integer>,
+        title -> Nullable<Text>,
+        create_time -> Nullable<Integer>,
+        update_time -> Nullable<Integer>,
+    }
+}
