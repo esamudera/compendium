51f96f0ec192706cba77f1c6f13a1d887c583a41
Ericko Samudera <ericko.samudera@gmail.com>
2020-11-22 14:39:06 +0700

    [feature] handle get list

--- src/playbook/list.rs
+++ src/playbook/list.rs
@@ -1,8 +1,20 @@
 use actix_web::{web, http, HttpRequest, HttpResponse, Result};
+use serde::Deserialize;
 
-pub async fn handle(req: HttpRequest) -> Result<HttpResponse> {
+#[derive(Deserialize)]
+pub struct QueryParams {
+    sort_by: Option<String>,
+    order: Option<String>,
+    limit: Option<i32>
+}
+
+pub async fn handle(query: web::Query<QueryParams>) -> Result<HttpResponse> {
+    let sort_by = query.sort_by.as_deref().unwrap_or("create_time");
+    let order = query.order.as_deref().unwrap_or("desc");
+    let limit : i32 = query.limit.unwrap_or(10);
+
     Ok(
         HttpResponse::Ok()
-            .finish()
+            .body(format!("BANG! {} {} {}", sort_by, order, limit))
     )
 }
