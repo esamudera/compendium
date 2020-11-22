8c1f2ddaf02e3c0c17ac35a626b9952c75c6e558
Ericko Samudera <ericko.samudera@gmail.com>
2020-11-22 14:54:41 +0700

    [feature] use enums

--- src/playbook/list.rs
+++ src/playbook/list.rs
@@ -1,6 +1,8 @@
 use actix_web::{web, http, HttpRequest, HttpResponse, Result};
 use serde::Deserialize;
 
+use std::fmt::Debug;
+
 #[derive(Deserialize)]
 pub struct QueryParams {
     sort_by: Option<String>,
@@ -8,13 +10,29 @@ pub struct QueryParams {
     limit: Option<i32>
 }
 
+#[derive(Debug)]
+enum SortOrder {
+    Ascending,
+    Descending
+}
+
 pub async fn handle(query: web::Query<QueryParams>) -> Result<HttpResponse> {
-    let sort_by = query.sort_by.as_deref().unwrap_or("create_time");
-    let order = query.order.as_deref().unwrap_or("desc");
-    let limit : i32 = query.limit.unwrap_or(10);
+    let input_sort_by = query.sort_by.as_deref().unwrap_or("create_time");
+    let input_order = query.order.as_deref().unwrap_or("desc");
+    let input_limit : i32 = query.limit.unwrap_or(10);
+
+    let order = enumify_order(input_order);
 
     Ok(
         HttpResponse::Ok()
-            .body(format!("BANG! {} {} {}", sort_by, order, limit))
+            .body(format!("BANG! {} {:?} {}", input_sort_by, order, input_limit))
     )
+}
+
+fn enumify_order(input_order: &str) -> SortOrder {
+    return match input_order {
+        "asc" => SortOrder::Ascending,
+        "desc" => SortOrder::Descending,
+        _ => SortOrder::Descending
+    };
 }
